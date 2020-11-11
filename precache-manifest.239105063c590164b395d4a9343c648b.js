e668ed169ae437a2421993ac868e9223e39fcd19
papi-root <papemorlo@hotmail.fr>
2020-11-11 13:08:20 +0000

    Updates

---
+++ precache-manifest.239105063c590164b395d4a9343c648b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "074ad81551942c0c5ac116e78a6bdd40",
+    "url": "/senservice/index.html"
+  },
+  {
+    "revision": "35c30f4c7303e3aa6510",
+    "url": "/senservice/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "8ac2dc7383fb94558c4a",
+    "url": "/senservice/static/css/main.8c17e15d.chunk.css"
+  },
+  {
+    "revision": "35c30f4c7303e3aa6510",
+    "url": "/senservice/static/js/2.6637061a.chunk.js"
+  },
+  {
+    "revision": "dbb0f42b90fdbe38353a1b5aa40c60bc",
+    "url": "/senservice/static/js/2.6637061a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ac2dc7383fb94558c4a",
+    "url": "/senservice/static/js/main.90bdda85.chunk.js"
+  },
+  {
+    "revision": "f8bde0f5c40e898f4a52",
+    "url": "/senservice/static/js/runtime-main.deee244b.js"
+  }
+]);

89d162a948071dadc0a9b7083a42f89465a8aa92
papi-root <papemorlo@hotmail.fr>
2020-11-12 18:12:04 +0000

    Updates

---
+++ precache-manifest.5120d51a9af643385aa4e4601068ece4.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d2d2409eca7da3f13ef596e6db92ded",
+    "url": "/seneservice/index.html"
+  },
+  {
+    "revision": "bfac765ba13c8c254942",
+    "url": "/seneservice/static/css/2.707b08b6.chunk.css"
+  },
+  {
+    "revision": "b1e2cbf1def2441a760f",
+    "url": "/seneservice/static/css/main.8c17e15d.chunk.css"
+  },
+  {
+    "revision": "bfac765ba13c8c254942",
+    "url": "/seneservice/static/js/2.5e9caf95.chunk.js"
+  },
+  {
+    "revision": "dbb0f42b90fdbe38353a1b5aa40c60bc",
+    "url": "/seneservice/static/js/2.5e9caf95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1e2cbf1def2441a760f",
+    "url": "/seneservice/static/js/main.2c3e0dfc.chunk.js"
+  },
+  {
+    "revision": "3fb5b26db8c4ee2bc2be",
+    "url": "/seneservice/static/js/runtime-main.9d5815d3.js"
+  },
+  {
+    "revision": "a9c40a900bb68842d64437f0cf35dd61",
+    "url": "/seneservice/static/media/01-small.a9c40a90.jpg"
+  },
+  {
+    "revision": "a6bf30dd0730004996e2a7751d88d0c9",
+    "url": "/seneservice/static/media/02-small.a6bf30dd.jpg"
+  },
+  {
+    "revision": "a985d8137584fe463a17f443fed32c3a",
+    "url": "/seneservice/static/media/03-small.a985d813.jpg"
+  },
+  {
+    "revision": "763950456862975f6e4a62a8d08f2b21",
+    "url": "/seneservice/static/media/04-small.76395045.jpg"
+  },
+  {
+    "revision": "684400d527a23436cb74980a2f95beb9",
+    "url": "/seneservice/static/media/05-small.684400d5.jpg"
+  },
+  {
+    "revision": "0b0da51a3377bd1b75979a4ffcfce822",
+    "url": "/seneservice/static/media/06-small.0b0da51a.jpg"
+  },
+  {
+    "revision": "1ae50217ea143c4f0ddd813b578e6456",
+    "url": "/seneservice/static/media/07-small.1ae50217.jpg"
+  },
+  {
+    "revision": "967c823fe6434f55576fd60284ec867e",
+    "url": "/seneservice/static/media/08-small.967c823f.jpg"
+  },
+  {
+    "revision": "09b22e7979d8080e874ed7dd34b069f4",
+    "url": "/seneservice/static/media/09-small.09b22e79.jpg"
+  },
+  {
+    "revision": "7b1cf8c40d4fb0cfd33ca0c6722c8e07",
+    "url": "/seneservice/static/media/about.7b1cf8c4.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/seneservice/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/seneservice/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/seneservice/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/seneservice/static/media/slick.f97e3bbf.svg"
+  }
+]);
